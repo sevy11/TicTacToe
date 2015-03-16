3713f97e26eeab0536c171bdbc906429ca67df30
Sevy11 <michaelsevy@gmail.com>
2015-03-15 22:32:57 -0500

    everything done but timer

---
+++ TicTacToe/WikipediaViewController.h
@@ -0,0 +1,13 @@
+//
+//  WikipediaViewController.h
+//  TicTacToe
+//
+//  Created by Michael Sevy on 3/13/15.
+//  Copyright (c) 2015 Michael Sevy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WikipediaViewController : UIViewController
+
+@end
---
+++ TicTacToe/WikipediaViewController.m
@@ -0,0 +1,37 @@
+//
+//  WikipediaViewController.m
+//  TicTacToe
+//
+//  Created by Michael Sevy on 3/13/15.
+//  Copyright (c) 2015 Michael Sevy. All rights reserved.
+//
+
+#import "WikipediaViewController.h"
+
+@interface WikipediaViewController ()
+
+@end
+
+@implementation WikipediaViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
